d39944bce609042868f6ecebe87a372dbfe3b11d
Erica  Cohen <ericacohen@ericas-air.attlocal.net>
2017-12-20 16:53:18 -0800

    twitter

--- keys.js
+++ keys.js
@@ -1,10 +1,24 @@
-var twitterKeys = {
+var keys = {
+   twitterKeys: {
     consumer_key: 'xQzbvzREv8gGJGOjATgXsGFSI',
     consumer_secret: 'UBJQ3rQUkelHCoTIWIfJ9JU8VgumRsDOYU13F0IOoBOT20FceO',
     access_token_key: '942903876247678976-lf39jq9RHaAirEiDJPneIYtcn4fOWSJ',
     access_token_secret: 'eV2rjwdPihhedpkoqSQrNuvcmjJWVKJwSQU92m8EcpDvM',
+  },
+  spotifyKeys: {
+    id: "30509a36ce5a4c8098ff8af25692ab72",
+    secret: "d004df95247b45819873a5a274e310f1"
   }
-  
-  module.exports = twitterKeys;
+} 
 
+// var twitterKeys = {
+//     consumer_key: 'xQzbvzREv8gGJGOjATgXsGFSI',
+//     consumer_secret: 'UBJQ3rQUkelHCoTIWIfJ9JU8VgumRsDOYU13F0IOoBOT20FceO',
+//     access_token_key: '942903876247678976-lf39jq9RHaAirEiDJPneIYtcn4fOWSJ',
+//     access_token_secret: 'eV2rjwdPihhedpkoqSQrNuvcmjJWVKJwSQU92m8EcpDvM',
+//   }
   
+  // module.exports = twitterKeys;
+
+  module.exports = keys;
+
--- liri.js
+++ liri.js
@@ -1,38 +1,75 @@
-var twitterKeys = require("./keys.js");
+var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 
-var spotify = new Spotify({
-    id: "30509a36ce5a4c8098ff8af25692ab72",
-    secret: "d004df95247b45819873a5a274e310f1"
-});
-
-spotify.search({
-    type: 'track',
-    query: 'All the Small Things'
-}, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-
-    console.log(data);
-});
-
-
+var spotify = new Spotify(keys.spotifyKeys);
+var client = new Twitter(keys.twitterKeys);
 
 
 var action = process.argv[2];
-var arguement = "";
+var arguement = process.argv[3];
 whatAction(action, arguement);
 
 // which action to take - using a swich
 function whatAction(action, arguement) {
     switch (action) {
         case "my-tweets":
-        // make function outside of switch and call it here
-        break;
+            myTweets();
+            break;
 
         case "spotify-this-song":
-        // make function outside of switch and call it here
-        break;
+            spotifyThisSong(arguement);
+            break;
+
+        case "movie-this":
+            movieThis(arguement);
+            break;
+
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+
     }
 }
+
+function myTweets() {
+    var params = {
+        screen_name: 'EricaCohen15'
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        
+        var forLoopLimit = 20;
+        if (tweets.length < 20) {
+            forLoopLimit = tweets.length;
+        }
+        
+        if (!error) {
+            for (var i=0; i<forLoopLimit; i++) {
+            console.log(tweets[i].created_at);
+            console.log(tweets[i].text);
+            }
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+function spotifyThisSong() {
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things'
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+    });
+};
+
+function movieThis() {
+
+};
+
+function doWhatItSays() {
+
+};
